cfea75b09606e766bd3e2e93bb75a270a666d229
TonyT <37555343+Tonyyytran@users.noreply.github.com>
2020-07-22 16:06:11 -0700

    upload

---
+++ cmake-build-debug/CMakeFiles/sockets.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sockets.dir/server.cpp.o"
+  "libsockets.dll.a"
+  "sockets.exe"
+  "sockets.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sockets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
